1b91d2be8f15b7c5bb23bcc3f2e943b67574fcd4
gmazy <mirohuttunen@nmimac6.nmi.jyu.fi>
2020-02-26 14:44:20 +0200

    test

---
+++ precache-manifest.668e13894aa4392d572969ee9e5852f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd9b112220779d5d5d90420cca549ddb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2baf1c2e56cc879974f0",
+    "url": "/static/css/main.44371d37.chunk.css"
+  },
+  {
+    "revision": "5da873b2e111a7625459",
+    "url": "/static/js/2.9dc34c33.chunk.js"
+  },
+  {
+    "revision": "efe80d506e138c343a03a1541511a2f3",
+    "url": "/static/js/2.9dc34c33.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2baf1c2e56cc879974f0",
+    "url": "/static/js/main.2e04eac7.chunk.js"
+  },
+  {
+    "revision": "696942293a92ac3a2bb6",
+    "url": "/static/js/runtime-main.889ad4b6.js"
+  }
+]);
